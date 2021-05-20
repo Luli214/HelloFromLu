7c24e8af87a90671450042b86e40f7c76989e0b7
Roman Ezequiel Alvarez Goyeneche <ralvarez.goyeneche@gmail.com>
2021-05-20 00:44:15 -0300

    New output message added

--- src/HelloLu.java
+++ src/HelloLu.java
@@ -1,5 +1,6 @@
 public class HelloLu {
     public static void main (String [] args){
         System.out.println("Hello, my name is Lu");
+        System.out.println("Hello team, I'm Lucia from another Github account")
     }
 }
